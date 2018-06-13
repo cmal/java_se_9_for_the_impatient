814ca4d341e28b70020b4a6e41910ce4152ce86f
Yu Zhao <yu.zhao@joudou.com>
2018-06-13 19:02:41 +0800

    3.13 part 2

--- ch03/ex/SortFiles.java
+++ ch03/ex/SortFiles.java
@@ -10,21 +10,16 @@ public class SortFiles {
         File f = new File(".");
         File[] files = f.listFiles();
 
-        Comparator<File> compCate = new Comparator<File>() {
+        Arrays.sort(files, new Comparator<File>() {
                 @Override
                 public int compare(File f1, File f2) {
                     int t1 = f1.isDirectory() ? 0 : 1;
                     int t2 = f2.isDirectory() ? 0 : 1;
                     return t1 - t2;
                 }
-            };
+            }.thenComparing((f1, f2) -> f1.getName().compareTo(f2.getName()))
+            );
 
-        Comparator<File> comp = compCate
-            .thenComparing((f1, f2) -> f1.getName().compareTo(f2.getName()));
-
-    
-
-        Arrays.sort(files, comp);
         for (File file : files) {
             System.out.println(file);
         }
