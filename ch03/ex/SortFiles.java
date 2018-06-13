809d69283f2f7a3308c0e028c439d78ff0273e84
Yu Zhao <yu.zhao@joudou.com>
2018-06-13 19:00:36 +0800

    3.13 part 1

--- ch03/ex/SortFiles.java
+++ ch03/ex/SortFiles.java
@@ -1,13 +1,33 @@
 package ch03.ex;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortFiles {
 
-    
-    
     public static void main(String[] args) {
         File f = new File(".");
         File[] files = f.listFiles();
+
+        Comparator<File> compCate = new Comparator<File>() {
+                @Override
+                public int compare(File f1, File f2) {
+                    int t1 = f1.isDirectory() ? 0 : 1;
+                    int t2 = f2.isDirectory() ? 0 : 1;
+                    return t1 - t2;
+                }
+            };
+
+        Comparator<File> comp = compCate
+            .thenComparing((f1, f2) -> f1.getName().compareTo(f2.getName()));
+
+    
+
+        Arrays.sort(files, comp);
+        for (File file : files) {
+            System.out.println(file);
+        }
+
     }
 }
