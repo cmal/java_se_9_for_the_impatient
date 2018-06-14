1d519ca861b237ec8afc795be507f8fa3bcedb17
Yu Zhao <zyzy5730@163.com>
2018-06-14 21:50:29 +0800

    4.9 part-1

--- ch04/ex/ClassMethods.java
+++ ch04/ex/ClassMethods.java
@@ -10,7 +10,15 @@ public class ClassMethods {
 
     public static void sixMethods(Object o) {
         Class c = o.getClass();
-        System.out.println(o.getClass().getName());
+        System.out.printf("Class: %s\n", c);
+        System.out.printf("getCanonicalName: %s\n", o.getClass().getCanonicalName());
+        System.out.printf("getName: %s\n", o.getClass().getName());
+        // System.out.println(o.getClass().getPackageName()); // java 9
+        System.out.printf("getSimpleName: %s\n", o.getClass().getSimpleName());
+        System.out.printf("getTypeName: %s\n", o.getClass().getTypeName());
+        System.out.printf("toGenericString: %s\n", o.getClass().toGenericString());
+        System.out.printf("toString: %s\n", o.getClass().toString());
+        System.out.println();
     }
     
     public static void main(String[] args) {
--- ch04/ex/UniversalToString.java
+++ ch04/ex/UniversalToString.java
@@ -2,25 +2,30 @@ package ch04.ex;
 
 import java.util.ArrayList;
 import java.lang.reflect.Field;
+import java.lang.reflect.Type;
 import ch04.ex.Point;
 import ch04.ex.Circle;
 
 public class UniversalToString {
-    public static ArrayList<String> universalToString(Object o) {
-        Class c = o.getClass();
+    public static ArrayList<String> universalToString(Class c) {
         Field[] fields = c.getDeclaredFields();
+        Class<?> superC = c.getSuperclass();
         ArrayList<String> fieldStrs = new ArrayList<String>();
+        if (superC.getName() != "java.lang.Object") {
+            for (String s : universalToString(superC)) {
+                fieldStrs.add(s);
+            }
+        }
         for (Field f : fields) {
             fieldStrs.add(f.getName());
         }
-        System.out.println(fields.length);
         return fieldStrs;
     }
 
     public static void main(String[] args) {
         Point p = new Point(3.5, 4);
-        Circle c = new Circle(p, 2.5);
-        for (String s : universalToString(c)) {
+        Circle circle = new Circle(p, 2.5);
+        for (String s : universalToString(circle.getClass())) {
             System.out.println(s);
         }
     }
