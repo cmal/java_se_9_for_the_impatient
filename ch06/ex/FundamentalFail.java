52d6b91e5f91e674f5b30de0bff470291aad8d61
Yu Zhao <yu.zhao@joudou.com>
2018-06-16 15:27:01 +0800

    6.20

--- ch06/ex/FundamentalFail.java
+++ ch06/ex/FundamentalFail.java
@@ -20,7 +20,22 @@ public class FundamentalFail {
         return result;
     }
 
+    @SafeVarargs
+    public static final <T> T[] repeat(int n, T... objs) {
+        ArrayList<T> result = new ArrayList<>();
+        for (int i = 0; i < n; i ++) {
+            for (T t : objs) {
+                result.add(t);
+            }
+        }
+        return result.toArray(objs);
+    }
+
     public static void main(String[] args) {
         Integer[] ints = repeat(10, 1, Integer[]::new);
+        Integer[] ints2 = repeat(3, 1, 2, 3);
+        for (Integer i : ints2) {
+            System.out.println(i);
+        }
     }
 }
