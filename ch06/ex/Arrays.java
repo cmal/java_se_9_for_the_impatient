09ef1d689482ab623f3902a0e16594be927cf895
Yu Zhao <yu.zhao@joudou.com>
2018-06-16 13:26:06 +0800

    6.9

--- ch06/ex/Arrays.java
+++ ch06/ex/Arrays.java
@@ -1,5 +1,8 @@
 package ch06.ex;
 
+import ch06.ex.Pair;
+import java.util.ArrayList;
+
 public class Arrays {
     // public static <T> void swap(T[] array, int i, int j) {
     //     T temp = array[i];
@@ -14,11 +17,23 @@ public class Arrays {
         return values;
     }
 
+    public static <E> Pair<E> firstLast(ArrayList<? super E> a) {
+        int len = a.size();
+        return new Pair(a.get(0), a.get(len - 1));
+    }
+
     public static void main(String[] args) {
         // String[] friends = new String[]{"A", "B", "C"};
         // Arrays.swap(friends, 0, 1);
         // System.out.printf("%s, %s, %s", friends[0], friends[1], friends[2]);
         Double[] result = Arrays.<Double>swap(0, 1, 1.5, 2.0, 3.0);
-        System.out.printf("%f, %f, %f", result[0], result[1], result[2]);
+        System.out.printf("%f, %f, %f\n", result[0], result[1], result[2]);
+
+        ArrayList<Double> arr = new ArrayList<Double>();
+        arr.add(1.5);
+        arr.add(2.0);
+        arr.add(3.0);
+        Pair<Double> p = firstLast(arr);
+        System.out.printf("%f, %f\n", p.getFirst(), p.getSecond());
     }
 }
--- ch06/ex/Pair.java
+++ ch06/ex/Pair.java
@@ -1,5 +1,6 @@
 package ch06.ex;
 
+// public class Pair<E extends Comparable> {
 public class Pair<E> {
     private E k;
     private E v;
@@ -17,11 +18,21 @@ public class Pair<E> {
         return v;
     }
 
+    // public E min() {
+    //     return k.compareTo(v) < 0 ? k : v;
+    // }
+
+    // public E max() {
+    //     return k.compareTo(v) < 0 ? v : k;
+    // }
+
     public static void main(String[] args) {
         String a = "A";
         String b = "B";
         Pair<String> p = new Pair<String>(a, b);
         System.out.println(p.getFirst());
         System.out.println(p.getSecond());
+        // System.out.println(p.min());
+        // System.out.println(p.max());
     }
 }
