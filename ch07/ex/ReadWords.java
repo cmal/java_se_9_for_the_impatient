2c066118c43606f213305664b870de47f87e1173
Yu Zhao <yu.zhao@joudou.com>
2018-06-17 14:04:21 +0800

    7-8

--- ch07/ex/ReadWords.java
+++ ch07/ex/ReadWords.java
@@ -6,9 +6,11 @@ import java.util.regex.*;
 
 public class ReadWords {
     private TreeMap<String, Integer> tm;
+    private TreeMap<String, ArrayList<Integer>> tml;
 
     ReadWords() {
         tm = new TreeMap<>();
+        tml = new TreeMap<>();
     }
 
     public void addWord(String s) {
@@ -20,10 +22,30 @@ public class ReadWords {
         }
     }
 
+    public void addWordLine(String s, int line) {
+        if (tml.containsKey(s)) {
+            ArrayList<Integer> v = tml.get(s);
+            v.add(line);
+            tml.put(s, v);
+        } else {
+            ArrayList<Integer> v = new ArrayList<>();
+            v.add(line);
+            tml.put(s, v);
+        }
+    }
+
     public void print() {
         for(Map.Entry<String,Integer> entry: tm.entrySet()) {
             String key = entry.getKey();
             Integer value = entry.getValue();
+            System.out.println(key + " => " + value);
+        }
+    }
+
+    public void printLine() {
+        for(Map.Entry<String, ArrayList<Integer>> entry: tml.entrySet()) {
+            String key = entry.getKey();
+            ArrayList<Integer> value = entry.getValue();
             System.out.println(key + " => " + value);
         }
     }
@@ -36,10 +58,13 @@ public class ReadWords {
         String line;
         String pattern = "(\\w+)";
         Pattern r = Pattern.compile(pattern);
+        int linum = 0;
         while ((line = br.readLine()) != null) {
+            linum ++;
             Matcher m = r.matcher(line);
             while(m.find()) {
                 addWord(m.group());
+                addWordLine(m.group(), linum);
             }
         }
     }
@@ -52,5 +77,6 @@ public class ReadWords {
             e.printStackTrace();
         }
         rw.print();
+        rw.printLine();
     }
 }
