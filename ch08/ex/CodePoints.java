4d93e843e0c915bfdf71bea566d5db45ebb2f7f5
Yu Zhao <yu.zhao@joudou.com>
2018-06-18 16:37:16 +0800

    8-7 part-1

--- ch08/ex/CodePoints.java
+++ ch08/ex/CodePoints.java
@@ -3,6 +3,7 @@ package ch08.ex;
 import java.util.*;
 import java.util.stream.*;
 import java.math.*;
+import java.io.*;
 import java.nio.*;
 import java.nio.charset.*;
 import java.nio.file.*;
@@ -20,19 +21,35 @@ public class CodePoints {
         return s.codePoints().reduce(0, (cur, next) -> Character.isAlphabetic(next) ? cur : cur + 1) == 0;
     }
 
+    public static <T> void print(Stream<T> stream) {
+        System.out.println(Arrays.asList(stream.toArray()));
+    }
+
     public static void main(String[] args) {
         try {
             String contents = new String(Files.readAllBytes(Paths.get("words.txt")),
                                          StandardCharsets.UTF_8);
             List<String> words = List.of(contents.split("\n"));
             var stream = words.stream().flatMap(w -> codePoints(w)).limit(100);
-            System.out.println(Arrays.asList(stream.toArray()));
+            print(stream);
+            System.out.println(isLetterOnlyWord("word"));
+            System.out.println(isLetterOnlyWord("中国"));
+            System.out.println(isLetterOnlyWord("not alphabetic"));
+
+            // 7. Turning a file into a stream of tokens, list the first
+            // 100 tokens that are words in the sense of the preceding
+            // exercise. Read the file again and list the 10 most frequent
+            // words, ignoring letter case.
+
+            var file = new File("words.txt");
+            var tokens = new Scanner(file).tokens(); // Stream<String>
+
+            print(tokens.filter(s -> isLetterOnlyWord(s)).limit(100));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        System.out.println(isLetterOnlyWord("word"));
-        System.out.println(isLetterOnlyWord("中国"));
-        System.out.println(isLetterOnlyWord("not alphabetic"));
+
     }
 }
