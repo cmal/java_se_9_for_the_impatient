3dda748aa89b7a7b90db67ca0a48fdbab2678342
Yu Zhao <yu.zhao@joudou.com>
2018-06-18 16:29:35 +0800

    8-6

--- ch08/ex/CodePoints.java
+++ ch08/ex/CodePoints.java
@@ -10,10 +10,14 @@ import java.nio.file.*;
 public class CodePoints {
 
     public static Stream<String> codePoints(String s) {
-        var offsetStream = IntStream.iterate(1,
+        var offsetStream = IntStream.iterate(0,
                                              i -> i < s.length(),
                                              i -> s.offsetByCodePoints(i, 1));
         return offsetStream.mapToObj(i -> s.substring(i, s.offsetByCodePoints(i, 1)));
+    }
+
+    public static boolean isLetterOnlyWord(String s) {
+        return s.codePoints().reduce(0, (cur, next) -> Character.isAlphabetic(next) ? cur : cur + 1) == 0;
     }
 
     public static void main(String[] args) {
@@ -26,5 +30,9 @@ public class CodePoints {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        System.out.println(isLetterOnlyWord("word"));
+        System.out.println(isLetterOnlyWord("中国"));
+        System.out.println(isLetterOnlyWord("not alphabetic"));
     }
 }
