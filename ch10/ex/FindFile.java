6884442ea99d59541a83e2ca4c247209fc4d681e
Yu Zhao <yu.zhao@joudou.com>
2018-06-26 11:19:25 +0800

    10-1 part 2 : find first

--- ch10/ex/FindFile.java
+++ ch10/ex/FindFile.java
@@ -7,30 +7,32 @@ import java.io.*;
 
 public class FindFile {
 
-//     “Using parallel streams, find all files in a directory that
-//     contain a given word. How do you find just the first one?
+    //     Using parallel streams, find all files in a directory that
+    //     contain a given word. How do you find just the first one?
 
-//    Are the files actually searched concurrently? TODO
+    //    Are the files actually searched concurrently? TODO
 
 
     public static void main(String[] args) {
         try {
-        Path path = Paths.get(".");
-        Stream<Path> sp = Files.walk(path);
-        sp.parallel()
-             // keep away from MalformedInputException
-            .filter(p -> p.toString().endsWith(".java"))
-            .filter(p -> {
-                if (Files.isRegularFile(p) && Files.isReadable(p)) {
-                    try(Stream<String> s = Files.lines(p)) {
-                        // System.out.println(p);
-                        return s.anyMatch(line -> line.contains("parallel"));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                return false;
-            }).forEach(System.out::println);
+            Path path = Paths.get(".");
+            Stream<Path> sp = Files.walk(path);
+            Optional<Path> presult = sp.parallel()
+                // keep away from MalformedInputException
+                .filter(p -> p.toString().endsWith(".java"))
+                .filter(p -> {
+                        if (Files.isRegularFile(p) && Files.isReadable(p)) {
+                            try(Stream<String> s = Files.lines(p)) {
+                                // System.out.println(p);
+                                return s.anyMatch(line -> line.contains("parallel"));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        return false;
+                    }).findFirst();
+
+            presult.ifPresent(System.out::println);
             
         } catch (IOException e) {
             e.printStackTrace();
